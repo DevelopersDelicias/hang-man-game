6f89834b76a4644b71592f4f874db9a46009b315
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2020-03-06 07:04:47 -0700

    ✅ Add second test to check the word is covered with _ symbol for each letter

--- src/game.js
+++ src/game.js
@@ -2,6 +2,10 @@ class Game {
   remainingPoints() {
     return 10;
   }
+
+  currentWord() {
+    return "_ _ _ _";
+  }
 }
 
 export default Game;
--- tests/game.test.js
+++ tests/game.test.js
@@ -3,11 +3,17 @@ import Game from "../src/game";
 describe("Game Rules", () => {
   it("when game starts user has 10 remaining points", () => {
     const game = new Game();
-
     const points = game.remainingPoints();
-
     const expected = 10;
-
     expect(points).toEqual(expected);
+  });
+
+  it("when game starts displays a _ for each letter in the selected word", () => {
+    const game = new Game("World");
+    const currentWord = game.currentWord();
+
+    const expected = "_ _ _ _";
+
+    expect(currentWord).toEqual(expected);
   });
 });
